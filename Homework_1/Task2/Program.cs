7fe619636d19391753e7a156f0dfaac978a42022
Anastasia <nast2209@yandex.ru>
2022-06-26 14:57:15 +0300

    Add Task2

---
+++ Homework_1/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿int a, b, c, max; 
+
+Console.Write("Input a first number: "); 
+a = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Input a second number: "); 
+b = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Input a third number: "); 
+c = Convert.ToInt32(Console.ReadLine()); 
+
+max = a; 
+
+if (b > max) 
+{ 
+    max = b; 
+} 
+
+if (c > max) 
+{ 
+    max = c; 
+} 
+
+Console.WriteLine("max = " + max); 
