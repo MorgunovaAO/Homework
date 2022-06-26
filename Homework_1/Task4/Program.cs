95f200daf32b6d484a11f14f188b72e30439add5
Anastasia <nast2209@yandex.ru>
2022-06-26 15:09:50 +0300

    Add Task4

---
+++ Homework_1/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n, current; 
+
+Console.Write ("Input a number: "); 
+n = Convert.ToInt32(Console.ReadLine()); 
+
+current = 2; 
+
+while (current <= n) 
+{ 
+    if (current % 2 == 0) 
+        Console.Write(current + " "); 
+    current ++; 
+} 
