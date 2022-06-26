9a6d1feffba09ab3278cef3b6e304fa0c792f5bc
Anastasia <nast2209@yandex.ru>
2022-06-26 14:38:33 +0300

    Add Task1

---
+++ Homework_1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int a, b; 
+
+Console.Write("Input a first number: "); 
+a = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Input a second number: "); 
+b = Convert.ToInt32(Console.ReadLine()); 
+
+if (a > b) 
+{ 
+    Console.WriteLine("max = " + a + ", min = " + b); 
+} 
+else 
+{ 
+    Console.WriteLine("max = " + b + ", min = " + a); 
+} 
