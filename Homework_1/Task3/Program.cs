bad1e0b67ee5024677a6668f9808ce03b2b58a9d
Anastasia <nast2209@yandex.ru>
2022-06-26 15:03:41 +0300

    Add Task3

---
+++ Homework_1/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int num; 
+
+Console.Write ("Input a number: "); 
+
+num = Convert.ToInt32(Console.ReadLine()); 
+
+if (num % 2 == 0) 
+{ 
+    Console.WriteLine(num + " is an even number."); 
+} 
+else 
+{ 
+    Console.WriteLine(num + " is an odd number."); 
+} 
