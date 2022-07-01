42d8a746e9cda1b28bf5396df78506c3567dce40
Anastasia <nast2209@yandex.ru>
2022-07-01 18:39:11 +0300

    add Homework_2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 1. Показать 2-ю цифру трехзн. числа:
+
+int CutNumber(int num)
+{
+    int des = (num % 100) / 10;
+    return des;
+}
+
+Console.Write("Input a number between 100 and 999: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(CutNumber (number));
+*/ 
+
+/* Задача 2. Показать третью слева цифру числа: 
+
+int ThirdPosition (int number)
+{
+    while (number > 1000) 
+        number = number / 10;
+    if (number > 99) number = number % 100 % 10;
+        else Console.WriteLine("The third position is absent");
+    return number;
+}
+
+Console.Write("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ThirdPosition(n));
+*/ 
+
+// Задача 3. Показать выходной: 
+
+void Weekend (int num)
+{
+    if (num == 6 || num == 7) Console.WriteLine("Yes");
+    if (num > 1 && num < 6) Console.WriteLine("No");
+    if (num < 1 || num > 7) Console.WriteLine("Uncorrect number!");
+}
+
+Console.WriteLine("Input a day's number: ");
+int day = Convert.ToInt32(Console.ReadLine());
+Weekend (day);
